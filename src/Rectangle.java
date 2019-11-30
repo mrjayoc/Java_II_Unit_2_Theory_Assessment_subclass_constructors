0481502f597487d04d0ea2be3c7fb68977d634c6
Matthew Johnson <mjohnson@oldcolony.int>
2019-11-30 14:10:31 -0500

    Initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle {
+
+    double width, height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return (2 * height) + (2 * width);
+    }
+}

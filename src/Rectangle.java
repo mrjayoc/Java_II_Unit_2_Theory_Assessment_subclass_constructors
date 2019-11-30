f3160efd1658fcff8f60db87306be27f58f4206c
mrjayoc <51881054+mrjayoc@users.noreply.github.com>
2019-11-30 14:58:14 -0500

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,6 +1,16 @@
 public class Rectangle {
 
     double width, height;
+    
+    public Rectangle() {
+        this.width = 0;
+        this.height = 0;
+    }
+    
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
 
     public double getArea() {
         return width * height;
